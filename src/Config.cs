617569db368748f479d39a4c19d4e9d50dbde32c
Catharina <catharina_@hotmail.com.br>
2023-03-30 19:49:19 -0300

    set up config file

---
+++ src/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx.Configuration;
+
+namespace MoreStatus
+{
+    public static class Config
+    {
+        private const string food_label = "Food Label";
+        public static ConfigEntry<bool> Food_Info;
+        private const string CTG_DETAILS = "Item Details Display Status Effect";
+        public static ConfigEntry<bool> cfg_Dets_Status_Effect;
+
+        public static void Init(ConfigFile config)
+        {
+            cfg_Dets_Status_Effect = config.Bind(CTG_DETAILS, "Display food status effects", true, "Item panel shows food status effects");
+        }
+    }
+
+}
