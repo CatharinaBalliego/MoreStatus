77b5ccd9a3c3c2f2d51c433fbabf4b093cb9b8ab
Catharina <catharina_@hotmail.com.br>
2023-04-27 21:04:06 -0300

    get status effect name

--- src/FoodStatus.cs
+++ src/FoodStatus.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using ItemDetailMoreStatus;
 using SideLoader;
 using System;
 using System.Collections.Generic;
@@ -10,69 +11,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using static ItemDetailsDisplay;
 
+
 namespace MoreStatus
 {
-    [HarmonyPatch(typeof(ItemDetailsDisplay), "RefreshDisplay")] // o metodo está errado.
+    [HarmonyPatch(typeof(ItemDetailsDisplay), "RefreshDisplay")]
 
     public class FoodStatus
     {
-        // static AccessTools.FieldRef<Item, string> food =
-        //AccessTools.FieldRefAccess<Item, string>("Description");
-        [HarmonyPostfix]
-        public static void RefreshDisplay(ItemDetailsDisplay __instance, IItemDisplay _itemDisplay)
+        [HarmonyPatch(typeof(ItemDetailsDisplay), "RefreshDetail")]
+        public class ItemDetailsDisplay_RefreshDetail
         {
-
-
-            if (Config.cfg_Dets_Status_Effect.Value)
+            [HarmonyPostfix]
+            public static void RefreshDetail(ItemDetailsDisplay __instance, int _rowIndex, DisplayedInfos _infoType)
             {
-                if (_itemDisplay.RefItem.IsPerishable && _itemDisplay.RefItem.CurrentDurability > 0)
+                Item m_lastItem = (Item)AccessTools.Field(typeof(ItemDetailsDisplay), "m_lastItem").GetValue(__instance);
+                if (m_lastItem.IsPerishable && m_lastItem.CurrentDurability > 0)
                 {
+                    ItemDetailRowDisplay row = (ItemDetailRowDisplay)AccessTools.Method(typeof(ItemDetailsDisplay), "GetRow").Invoke(__instance, new object[] { _rowIndex });
+                    Text m_lblDataName = (Text)AccessTools.Field(typeof(ItemDetailRowDisplay), "m_lblDataName").GetValue(row);
+
                     List<ItemDetailRowDisplay> m_detailRows = (List<ItemDetailRowDisplay>)AccessTools.Field(typeof(ItemDetailsDisplay), "m_detailRows").GetValue(__instance);
-                    ItemDetailRowDisplay row = (ItemDetailRowDisplay)AccessTools.Method(typeof(ItemDetailsDisplay), "GetRow").Invoke(__instance, new object[] { m_detailRows.Count });
-                    //ItemDetailRowDisplay row = (ItemDetailRowDisplay)AccessTools.Method(typeof(ItemDetailsDisplay), "GetRow").Invoke(__instance, new object[] { m_detailRows.Count });
-                    row.SetInfo(LocalizationManager.Instance.GetLoc("ItemStat_Effect"), $"{_itemDisplay.RefItem.m_effects}");
-                    //  row.SetInfo(LocalizationManager.Instance.GetLoc("ItemStat_Durability"), $"{_itemDisplay.RefItem.CurrentDurability}/{_itemDisplay.RefItem.MaxDurability}");
 
-                }
-            }
-        }
-        //public static void test()
-        //{
-        //    StatusData item = new StatusData(this.StatusData);
-        //    this.m_statusStack.Add(item);
-        //    this.m_totalData = new StatusData.EffectData[this.StatusData.EffectsData.Length];
-        //    for (int i = 0; i < this.m_totalData.Length; i++)
-        //    {
-        //        this.m_totalData[i].Data = new string[this.StatusData.EffectsData[i].Data.Length];
-        //    }
-        //}
+                    if (m_lastItem.IsFood)
+                    {
+                        //row.SetInfo(LocalizationManager.Instance.GetLoc("ItemStat_Durability"), $"{m_lastItem.m_effects}");
+                        row.SetInfo("Effect", "test");
 
-
-    }
-
-    [HarmonyPatch(typeof(ItemDetailsDisplay), "RefreshDetail")]
-    public class ItemDetailsDisplay_RefreshDetail
-    {
-        [HarmonyPostfix]
-        public static void RefreshDetail(ItemDetailsDisplay __instance, int _rowIndex, DisplayedInfos _infoType)
-        {
-            //if (!Config.cfg_Dets_Status_Effect.Value)
-            //{
-            //    return;
-            //}
-
-            Item m_lastItem = (Item)AccessTools.Field(typeof(ItemDetailsDisplay), "m_lastItem").GetValue(__instance);
-
-            if (m_lastItem.IsPerishable && m_lastItem.CurrentDurability > 0)
-            {
-                ItemDetailRowDisplay row = (ItemDetailRowDisplay)AccessTools.Method(typeof(ItemDetailsDisplay), "GetRow").Invoke(__instance, new object[] { _rowIndex });
-                Text m_lblDataName = (Text)AccessTools.Field(typeof(ItemDetailRowDisplay), "m_lblDataName").GetValue(row);
-                if (m_lastItem.IsFood)
-                {
-                    row.SetInfo(LocalizationManager.Instance.GetLoc("ItemStat_Effect"), $"{m_lastItem.m_effects}");
+                        foreach (var effect in m_lastItem.m_effects)
+                        {
+                            if(effect.Value.Effect.GetType() == typeof(AddStatusEffect))
+                            {
+                                var castTypeEffect = (AddStatusEffect)effect.Value.Effect;
+                                ItemDetailMoreStatus.MoreStatus.Log.LogMessage($"IdentifierName: {castTypeEffect.Status.IdentifierName}");
+                                ItemDetailMoreStatus.MoreStatus.Log.LogMessage($"Description: {castTypeEffect.Status.Description}");
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-
 }
